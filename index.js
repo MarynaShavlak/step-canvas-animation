eba8fbd21cf0a0f76415c92d84f71c448beabddb
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-02-04 20:48:22 +0200

    add canvas classes

--- index.js
+++ index.js
@@ -1,105 +1,124 @@
 class Circle {
-  constructor(x, y, radius, speedX, speedY, color, opacity) {
+  constructor(x, y, radius, velocity, color, opacityModifier) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.speedX = speedX;
-    this.speedY = speedY;
+    this.velocity = { x: velocity.x, y: velocity.y };
     this.color = color;
-    this.opacity = opacity;
+    this.alpha = 1;
+    this.opacityModifier = opacityModifier;
+    this.canvas = document.querySelector('.canvas');
   }
-  showCircle() {
+
+  draw(ctx) {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     ctx.strokeStyle = this.color;
     ctx.fillStyle = this.color;
-
     ctx.stroke();
     ctx.fill();
+    ctx.restore();
   }
-  moveCircle() {
-    if (
-      this.x + this.radius > canvas.width ||
-      this.x - this.radius < 0 ||
-      this.y > canvas.height
-    ) {
+
+  move() {
+    if (this.isOutOfBounds()) {
       this.resetCircle();
     }
-    this.x += this.speedX;
-    this.y += this.speedY;
-    ctx.strokeStyle = `rgba(${hexToRgb(this.color)}, ${this.opacity})`;
-    ctx.fillStyle = `rgba(${hexToRgb(this.color)}, ${this.opacity})`;
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+
+  update(ctx) {
+    this.draw(ctx);
+    this.move();
+    this.updateAlpha();
+  }
+
+  isOutOfBounds() {
+    return (
+      this.x + this.radius > this.canvas.width ||
+      this.x - this.radius < 0 ||
+      this.y > this.canvas.height
+    );
+  }
+
+  updateAlpha() {
+    this.alpha =
+      Math.abs(Math.sin(Date.now() * this.opacityModifier)) * 0.5 + 0.5;
   }
 
   resetCircle() {
-    this.x = Math.random() * (canvas.width - this.radius * 2) + this.radius;
+    this.x =
+      Math.random() * (this.canvas.width - this.radius * 2) + this.radius;
     this.y = -this.radius;
   }
 }
 
-const canvas = document.querySelector('.canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+class CanvasAnimation {
+  constructor() {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d');
+    this.resizeCanvas();
+    this.colorsArr = ['#343434', '#1c1c1c', '#6c6c6c', '#575757', '#808080'];
+    this.opacityModifiers = [0.001, 0.002, 0.003, 0.004, 0.005];
+    this.circlesArr = this.createCircles(160);
 
-const colorsArr = ['#343434', '#1c1c1c', '#6c6c6c', '#575757', '#808080'];
-const circlesArr = createCircles(160);
+    window.addEventListener('resize', () => {
+      this.resizeCanvas();
+      this.resetAllCircles();
+    });
 
-function createCircles(num) {
-  return Array.from({ length: num }, () => {
-    const radius = randomNumber(3, 10);
-    const x = Math.random() * (canvas.width - radius * 2) + radius;
-    const y = Math.random() * (canvas.height - radius * 2) + radius;
-    const speedX = 0.2;
-    const speedY = (Math.random() + 0.1) * 5;
-    const color = colorsArr[randomNumber(0, colorsArr.length - 1)];
-    const opacity = 0.1;
-    return new Circle(x, y, radius, speedX, speedY, color, opacity);
-  });
-}
+    this.initAnimation();
+  }
 
-function resetAllCircles() {
-  circlesArr.forEach(circle => {
-    circle.x =
-      Math.random() * (canvas.width - circle.radius * 2) + circle.radius;
-    circle.y =
-      Math.random() * (canvas.height - circle.radius * 2) + circle.radius;
-  });
-}
+  resizeCanvas() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
 
-function initAnimation() {
-  requestAnimationFrame(initAnimation);
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  circlesArr.forEach((circle, index) => {
-    circle.opacity = 0.5 + 0.5 * Math.sin((index + 1) * 0.05);
-    circle.showCircle();
-    circle.moveCircle();
-  });
+  createCircles(num) {
+    return Array.from({ length: num }, () => {
+      const radius = randomNumber(5, 20);
+      const x = Math.random() * (this.canvas.width - radius * 2) + radius;
+      const y = Math.random() * (this.canvas.height - radius * 2) + radius;
+      const velocity = { x: 0.2, y: (Math.random() + 0.1) * 5 };
+      const color = this.colorsArr[randomNumber(0, this.colorsArr.length - 1)];
+      const opacityModifier =
+        this.opacityModifiers[
+          randomNumber(0, this.opacityModifiers.length - 1)
+        ];
+      return new Circle(x, y, radius, velocity, color, opacityModifier);
+    });
+  }
+
+  resetAllCircles() {
+    this.circlesArr.forEach(circle => {
+      circle.x =
+        Math.random() * (this.canvas.width - circle.radius * 2) + circle.radius;
+      circle.y =
+        Math.random() * (this.canvas.height - circle.radius * 2) +
+        circle.radius;
+    });
+  }
+
+  initAnimation() {
+    requestAnimationFrame(() => this.initAnimation());
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.circlesArr.forEach(circle => {
+      if (circle.alpha <= 0) {
+        circle.alpha = 1;
+      }
+      circle.update(this.ctx);
+    });
+  }
 }
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function hexToRgb(hex) {
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-  hex = hex.replace(shorthandRegex, (m, r, g, b) => {
-    return r + r + g + g + b + b;
-  });
-
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(
-        result[3],
-        16,
-      )}`
-    : null;
-}
-
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  resetAllCircles();
-});
-
-initAnimation();
+const canvas = document.querySelector('.canvas');
+const canvasAnimation = new CanvasAnimation(canvas);
